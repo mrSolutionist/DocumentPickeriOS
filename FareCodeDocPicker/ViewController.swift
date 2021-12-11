158172fd425342953de7fa78e3228004ed7291d0
ROBIN GEORGE <therobins.dev@gmail.com>
2021-12-11 18:47:24 +0530

    final updates

--- FareCodeDocPicker/ViewController.swift
+++ FareCodeDocPicker/ViewController.swift
@@ -11,7 +11,7 @@ import QuickLook
 
 
 class ViewController: UIViewController{
-
+    
     //table veiw
     @IBOutlet weak var tableView: UITableView!
     
@@ -26,13 +26,14 @@ class ViewController: UIViewController{
         
         tableView.delegate = self
         tableView.dataSource = self
-
+        
     }
     
+    //MARK: upload button
     @IBAction func uploadButton(_ sender: Any) {
         
         //setting the types of extentions that wants to be read also creates a copy of file
-        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.png,UTType.jpeg, UTType.pdf,UTType.svg,UTType.epub,UTType.gif],asCopy: true)
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [UTType.png,UTType.jpeg, UTType.pdf,UTType.svg,UTType.epub,UTType.gif,UTType.text],asCopy: true)
         documentPicker.delegate = self
         documentPicker.allowsMultipleSelection = false
         documentPicker.modalPresentationStyle = .fullScreen
@@ -40,22 +41,20 @@ class ViewController: UIViewController{
         
     }
     
+    //MARK: alert pop up
     func alert(string :String) {
         let alert = UIAlertController(title:string , message: nil,         preferredStyle: UIAlertController.Style.alert)
-
+        
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { _ in
-                //Cancel Action
-            }))
-           
-            
-            self.present(alert, animated: true, completion: nil)
-        }
-    
-    
-    
+            //Cancel Action
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
     
 }
 
+
+//MARK: Document picker delegate
 extension ViewController:UIDocumentPickerDelegate {
     func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
         guard let url = urls.first else {return}
@@ -63,7 +62,7 @@ extension ViewController:UIDocumentPickerDelegate {
         let fileUrl  = directoryUrl.appendingPathComponent(url.lastPathComponent)
         
         if fileUrl.hasDirectoryPath{
-            alert(string: "File Already Exists")
+            
             
         }
         else  {
@@ -76,8 +75,8 @@ extension ViewController:UIDocumentPickerDelegate {
                 tableView.reloadData()
             }
             catch {
-               
-                print("copy error")
+                
+                alert(string: "File Already Exists")
             }
             
         }
@@ -86,6 +85,7 @@ extension ViewController:UIDocumentPickerDelegate {
     }
 }
 
+//MARK: Preview controller delegate
 extension ViewController: QLPreviewControllerDataSource {
     func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
         return directoryContents.count
@@ -96,22 +96,19 @@ extension ViewController: QLPreviewControllerDataSource {
     }
 }
 
+
+//MARK: table view data source
 extension ViewController: UITableViewDataSource {
-    
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         do {
             // Get the directory contents urls (including subfolders urls)
             directoryContents = try FileManager.default.contentsOfDirectory(at: directoryUrl, includingPropertiesForKeys: nil)
-            print(directoryContents!)
-            
             return directoryContents.count
-
+            
         } catch {
             print(error)
         }
-       return 0
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -121,9 +118,23 @@ extension ViewController: UITableViewDataSource {
         
         return cell
     }
-}
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        //deleting swipe
+        let delete = UIContextualAction(style: .destructive, title: "delete"){ [weak self]
+            (action,view,completionHandler) in
+            try! FileManager.default.removeItem(at: self!.directoryContents[indexPath.row].absoluteURL)
+                //refresh
+                tableView.reloadData()
+            }
+            return  UISwipeActionsConfiguration (actions: [delete])
+        }
+    }
+    
 
 
+//MARK: table view data delegate
 extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -135,6 +146,7 @@ extension ViewController: UITableViewDelegate {
         quickLookViewController.currentPreviewItemIndex = indexPath.row
         // 4
         present(quickLookViewController, animated: true)
-
+        
     }
+    
 }
